fedcca788deddd6b007c971b74680a9d08d723ca
Tarikul Islam <127613853+Tarik0011@users.noreply.github.com>
2023-03-23 10:42:23 +0600

    Update Oliver and the Game(Hackerearth. solution).cpp

--- Queries On Tree/Eulear Tour/Oliver and the Game(Hackerearth. solution).cpp
+++ Queries On Tree/Eulear Tour/Oliver and the Game(Hackerearth. solution).cpp
@@ -1,4 +1,6 @@
 //LINK:https://www.hackerearth.com/practice/algorithms/graphs/topological-sort/practice-problems/algorithm/oliver-and-the-game-3/
+
+//Just subtree check
 /*
 *   -------------- ?*?*? --------------
 * |         In The Name of *Allah*     |
