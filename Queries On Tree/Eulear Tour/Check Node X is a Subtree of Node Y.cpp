dae1c8ca747f92fd9b2392b06bfb755358c8b8f3
Tarikul Islam <127613853+Tarik0011@users.noreply.github.com>
2023-03-23 10:24:43 +0600

    Create Check Node X is a Subtree of Node Y.cpp

---
+++ Queries On Tree/Eulear Tour/Check Node X is a Subtree of Node Y.cpp
@@ -0,0 +1,62 @@
+
+/*
+*   -------------- ?*?*? --------------
+* |         In The Name of *Allah*     |
+* |             Author : Tarik         |
+* |                                    |
+*  -------------- ?*?*? ---------------
+*/
+ 
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define  pb        push_back
+#define  all(v)    v.begin(),v.end()
+#define  endl       '\n'  
+#define  uint       long long
+#define  int        long long
+#define  BLOCK        700
+#define  fastio    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set; 
+
+const int mxn=1e6+123;
+vector<int>adj[mxn];
+int S[mxn],T[mxn],FT[2*mxn],timer=1;
+
+void dfs(int node,int par){
+	S[node]=timer;
+	FT[timer]=node;
+	timer++;
+	for(auto u:adj[node]){
+		if(u!=par){
+			dfs(u,node);
+		}
+	}
+	T[node]=timer;
+	FT[timer]=node;
+	timer++;
+}
+void solve(){
+	int n;
+	cin>>n;
+	for(int i=1;i<=n-1;i++){
+		int u,v;
+		cin>>u>>v;
+		adj[u].pb(v);
+		adj[v].pb(u);
+	}
+	dfs(1,-1);
+	int x,y;
+	cin>>x>>y;
+	if(S[x]>S[y] and T[x]<T[y])cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+}
+signed main(){ 
+    fastio; 
+    int t=1;
+   // cin>>t;
+    while(t--){
+       solve();
+    }
+}
