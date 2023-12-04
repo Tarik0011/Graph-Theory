2ddc1685f47723301c26dfdfd378fe1e56563569
Tarikul Islam <127613853+Tarik0011@users.noreply.github.com>
2023-12-04 12:50:52 +0600

    Create implementation.cpp

---
+++ Floyd Warshall Algorithm(All pair Shortest Path)/implementation.cpp
@@ -0,0 +1,70 @@
+/*
+*   -------------- ?*?*? --------------
+* |         In The Name of *Allah*     |
+* |             Author : Tarik         |
+* |                                    |
+*  -------------- ?*?*? ---------------
+*/
+ 
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define endl '\n'
+#define uint long long
+#define int long long
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+
+const int mxn=505;
+int dis[mxn][mxn];
+void solve() {
+	int n,m;
+	cin>>n>>m;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			if(i==j)continue;
+			dis[i][j]=1e18;
+		}
+	}
+	for(int i=1;i<=m;i++){
+		int u,v,w;
+		cin>>u>>v>>w;
+		dis[u][v]=w;
+	}
+
+	for(int k=1;k<=n;k++){
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=n;j++){
+				if(dis[i][k]+dis[k][j]<dis[i][j]){
+					dis[i][j]=dis[i][k]+dis[k][j];
+				}
+			}
+		}
+	}
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			if(dis[i][j]!=1e18){
+				cout<<dis[i][j]<<" ";
+			}else{
+				cout<<"inf"<<" ";
+			}
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+}
+signed main(){
+    fastio;
+    int t = 1;
+    // cin >> t;
+    for(int i = 1; i <= t; i++){
+        // cout << "Case " << i << ": ";
+        solve();
+    }
+    return 0;
+}
